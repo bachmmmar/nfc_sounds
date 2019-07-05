761e0fce6ed264363e15d654cc38fb2402ccdbf0
Martin Bachmann <bachmmar@gmail.com>
2019-07-05 21:58:41 +0200

    Introduction screen with helper video

--- app/src/main/java/com/bachmann/nfcsound/IntroductionActivity.java
+++ app/src/main/java/com/bachmann/nfcsound/IntroductionActivity.java
@@ -4,6 +4,8 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.nfc.NfcAdapter;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -11,6 +13,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Switch;
 import android.widget.Toast;
+import android.widget.VideoView;
 
 public class IntroductionActivity extends AppCompatActivity {
 
@@ -18,6 +21,7 @@ public class IntroductionActivity extends AppCompatActivity {
 
     private AppStatus status;
     private NfcAdapter nfcAdapter;
+    private VideoView helpVideo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +31,8 @@ public class IntroductionActivity extends AppCompatActivity {
 
         status = ((AppContainer) getApplicationContext()).getStatus();
         nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        helpVideo = findViewById(R.id.videoView);
+        setupVideoView();
 
         isNfcSupportedOnDevice();
     }
@@ -38,6 +44,38 @@ public class IntroductionActivity extends AppCompatActivity {
         updateViewButtons();
     }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        hideVideoView();
+    }
+
+    private void setupVideoView() {
+        hideVideoView();
+        helpVideo.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mediaPlayer) {
+                hideVideoView();
+            }
+        });
+    }
+
+    private void hideVideoView() {
+        if (!helpVideo.isPlaying()) {
+            helpVideo.stopPlayback();
+        }
+        helpVideo.setVisibility(View.INVISIBLE);
+    }
+
+    public void showNFCHint() {
+        if (!helpVideo.isPlaying()) {
+            helpVideo.setVisibility(View.VISIBLE);
+            String path = "android.resource://" + getPackageName() + "/" +  R.raw.help_enable_nfc;
+            helpVideo.setVideoURI(Uri.parse(path));
+            helpVideo.start();
+        }
+        Toast.makeText(this, getString(R.string.NFC_disabled), Toast.LENGTH_LONG).show();
+    }
 
     private boolean isNfcSupportedOnDevice() {
         if (nfcAdapter == null) {
@@ -80,8 +118,7 @@ public class IntroductionActivity extends AppCompatActivity {
 
     public void onClickStartExplorerGame(View v) {
         if (!nfcAdapter.isEnabled()) {
-            Toast.makeText(this, getString(R.string.NFC_disabled),
-                    Toast.LENGTH_LONG).show();
+            showNFCHint();
             return;
         }
 
@@ -90,8 +127,7 @@ public class IntroductionActivity extends AppCompatActivity {
 
     public void onClickWriteCard(View v) {
         if (!nfcAdapter.isEnabled()) {
-            Toast.makeText(this, getString(R.string.NFC_disabled),
-                    Toast.LENGTH_LONG).show();
+            showNFCHint();
             return;
         }
 
